a3b8b8fb7f45ddb5337a7756a8f01175903b57c0
IsmailParacha <ismailjanparacha@gmail.com>
2022-07-15 18:52:31 +0500

    commit

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -155,8 +155,6 @@ class _signupState extends State<signup> {
                       child: RaisedButton(
                         onPressed: () {
                           if (_formkey.currentState!.validate()) {
-                            // If the form is valid, display a snackbar. In the real world,
-                            // you'd often call a server or save the information in a database.
                             ScaffoldMessenger.of(context).showSnackBar(
                               const SnackBar(content: Text('Processing Data')),
                             );
